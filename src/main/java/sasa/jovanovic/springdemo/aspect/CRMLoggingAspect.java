ad5d0dbaf7ba409280905f6f33c5e92d98cdb853
sjovanovic974 <sjovanovic974@gmail.com>
2021-11-06 11:40:35 +0100

    @AfterReturning advice added

--- src/main/java/sasa/jovanovic/springdemo/aspect/CRMLoggingAspect.java
+++ src/main/java/sasa/jovanovic/springdemo/aspect/CRMLoggingAspect.java
@@ -3,6 +3,7 @@ package sasa.jovanovic.springdemo.aspect;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
@@ -50,25 +51,15 @@ public class CRMLoggingAspect {
 	}
 	
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 	// add @AfterReturning advice
+	@AfterReturning(pointcut="forAppFlow()", returning = "theResult" )
+	public void afterReturning(JoinPoint theJoinPoint, Object theResult) {
+		
+		// display method we are returning from
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("======>> in @AfterReturning: calling method: " + theMethod);
+		
+		// display data returned
+		myLogger.info("======>> result: " + theResult);
+	}	
 }
