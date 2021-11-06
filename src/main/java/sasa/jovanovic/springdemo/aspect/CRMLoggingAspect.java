69777c19768a25c6f2a05b8765b280fd45a09fe3
sjovanovic974 <sjovanovic974@gmail.com>
2021-11-06 11:28:43 +0100

    @Before advice added

--- src/main/java/sasa/jovanovic/springdemo/aspect/CRMLoggingAspect.java
+++ src/main/java/sasa/jovanovic/springdemo/aspect/CRMLoggingAspect.java
@@ -2,7 +2,9 @@ package sasa.jovanovic.springdemo.aspect;
 
 import java.util.logging.Logger;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -14,20 +16,59 @@ public class CRMLoggingAspect {
 	private Logger myLogger = Logger.getLogger(getClass().getName());
 	
 	// setup pointcut declarations
-	@Pointcut("execution(* sasa.jovanovic.springdemo.controller.*.*(..)")
+	@Pointcut("execution(* sasa.jovanovic.springdemo.controller.*.*(..))")
 	private void forControllerPackage() {}
 	
 	// do the same for service and dao
-	@Pointcut("execution(* sasa.jovanovic.springdemo.service.*.*(..)")
+	@Pointcut("execution(* sasa.jovanovic.springdemo.service.*.*(..))")
 	private void forServicePackage() {}
 	
-	@Pointcut("execution(* sasa.jovanovic.springdemo.dao.*.*(..)")
+	@Pointcut("execution(* sasa.jovanovic.springdemo.dao.*.*(..))")
 	private void forDaoPackage() {}
 	
 	@Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
 	private void forAppFlow() {}
 	
+	
 	// add @Before advice
+	@Before("forAppFlow()")
+	public void before(JoinPoint theJoinPoint) {
+		
+		// display methods we are calling
+		String theMethod = theJoinPoint.getSignature().toShortString();
+		myLogger.info("======>> in @Before: calling method: " + theMethod);
+		
+		// display the arguments to the method
+		
+		// get the arguments
+		Object[] args = theJoinPoint.getArgs();
+		
+		// loop through and display args
+		for(Object tempArg : args){
+			myLogger.info("=====>> argument: " + tempArg);
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	// add @AfterReturning advice
 }
